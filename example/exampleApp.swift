5941d8836d5bfb521df1c3ab9fe8908d230439ff
Ethan Morgan <ethan@gweithio.com>
2024-02-29 16:36:35 +0000

    Initial Commit

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Ethan Morgan on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
