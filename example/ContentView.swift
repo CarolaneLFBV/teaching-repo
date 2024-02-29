aef45f59884ed49d7b7d3a088c43575ea407efff
Ethan Morgan <ethan@gweithio.com>
2024-02-29 19:25:59 +0000

    init

--- example/ContentView.swift
+++ example/ContentView.swift
@@ -7,18 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum TodoStatus: String {
+    case pending
+    case done
+}
+
+struct Todo: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let status: TodoStatus
+}
+
+var todos: [Todo] = [
+    Todo(title: "Buy groceries", status: .pending),
+    Todo(title: "Finish homework", status: .pending),
+]
+
+struct StatusIndicator: View {
+    var status: TodoStatus
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        let backgroundColor: Color = {
+            switch status {
+            case .done:
+                return Color.green
+            case .pending:
+                return Color.orange
+            }
+        }()
+
+        Text(status == .done ? "Done" : "Pending")
+            .font(.footnote)
+            .foregroundColor(.white)
+            .padding(8)
+            .background(backgroundColor)
+            .clipShape(Capsule())
+        
     }
 }
 
-#Preview {
-    ContentView()
+struct AddTodoForm: View {
+    @State private var todoTitle: String = ""
+    
+    private func addTodo() {
+        todos.append(Todo(title: todoTitle, status: .pending))
+    }
+    
+    var body: some View {
+        Form {
+            TextField("New Todo", text: $todoTitle)
+            Button(action: addTodo) {
+                Text("Add")
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            List(todos) { todo in
+                Text(todo.title)
+                    .font(.title)
+                StatusIndicator(status: todo.status)
+            }
+            AddTodoForm()
+        }
+        .padding()
+        .navigationTitle("Todo Details")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
